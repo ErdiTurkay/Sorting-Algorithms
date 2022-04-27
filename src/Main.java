41542bda7c5f9ef047da5ac48e69e8e62f80974f
Erdi <erditurkay@gmail.com>
2022-04-27 17:03:03 +0300

    * The left-most element is taken as the pivot element in the quicksort algorithm.

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,8 @@ public class Main {
         Scanner scanner= new Scanner(System.in);
         int k;
 
+        // Gets the value of K from the user to find the kthSmallestValue.
+        // Forces the user to enter an integer value greater than or equal to 1.
         while(true){
             System.out.print(">> Give k Value for kthSmallest (k>=1): ");
 
@@ -39,28 +41,34 @@ public class Main {
             }
         }
 
-
+        // Measures the speed of each sorting algorithm for array with 10 indexes.
         System.out.println("\n<< Results of Array with 10 Indexes (ms) >>");
         calculateTime(ten, tmpTen, k);
         System.out.println("\n");
 
+        // Measures the speed of each sorting algorithm for array with 100 indexes.
         System.out.println("<< Results of Array with 100 Indexes (ms) >>");
         calculateTime(hundred, tmpHundred, k);
         System.out.println("\n");
 
+        // Measures the speed of each sorting algorithm for array with 1000 indexes.
         System.out.println("<< Results of Array with 1000 Indexes (ms) >>");
         calculateTime(thousand, tmpThousand, k);
         System.out.println("\n");
 
+        // Measures the speed of each sorting algorithm for array with 10 Thousand indexes.
         System.out.println("<< Results of Array with 10 Thousand Indexes (ms) >>");
         calculateTime(tenThousand, tmpTenThousand, k);
         System.out.println("\n");
 
+        // Measures the speed of each sorting algorithm for array with 100 Thousand indexes.
         System.out.println("<< Results of Array with 100 Thousand Indexes (ms) >>");
         calculateTime(hundredThousand, tmpHundredThousand, k);
         System.out.println("\n");
     }
 
+    // Creates random numbers between 1 and 10 times the length parameter it takes,
+    // and creates an array in the size of the parameter it receives.
     public static int[] createArray(int length){
         int[] array = new int[length];
 
@@ -70,10 +78,12 @@ public class Main {
         return array;
     }
 
+    // Copies an array with its contents into another array.
     private static void copyArray(int[] temp, int[] array){
         System.arraycopy(array, 0, temp, 0, array.length);
     }
 
+    // Tests the array parameter it receives in all sorting algorithms and returns its measurements.
     public static void calculateTime(int[] array, int[] tmpArray, int k){
         long startTime;
         String totalTime;
@@ -90,7 +100,7 @@ public class Main {
             System.out.print("Quick Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: "+kthSmallest+"\n");
         else
             System.out.print("Quick Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: Out of Bounds Array\n");
-
+        //////////////////////////////
 
         // HEAP SORT ALGORITHM
         copyArray(tmpArray,array);
@@ -102,7 +112,7 @@ public class Main {
             System.out.print("Heap Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: "+kthSmallest+"\n");
         else
             System.out.print("Heap Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: Out of Bounds Array\n");
-
+        //////////////////////////////
 
         // MERGE SORT ALGORITHM
         copyArray(tmpArray,array);
@@ -114,7 +124,7 @@ public class Main {
             System.out.print("Merge Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: "+kthSmallest+"\n");
         else
             System.out.print("Merge Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: Out of Bounds Array\n");
-
+        //////////////////////////////
 
         // INSERTION SORT ALGORITHM
         copyArray(tmpArray,array);
@@ -126,6 +136,7 @@ public class Main {
             System.out.print("Insertion Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: "+kthSmallest+"\n");
         else
             System.out.print("Insertion Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: Out of Bounds Array\n");
+        //////////////////////////////
 
 
         // SELECTION SORT ALGORITHM
@@ -138,6 +149,7 @@ public class Main {
             System.out.print("Selection Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: "+kthSmallest+"\n");
         else
             System.out.print("Selection Sort: \n\t" + totalTime + " ms | " + k +  "th Smallest Element: Out of Bounds Array\n");
+        //////////////////////////////
     }
 
     //////////////////////////////////////
@@ -157,6 +169,7 @@ public class Main {
 
     static int insertionSort(int array[], int k) {
         int length = array.length, temporary, j;
+
         for (int i = 1; i < length; i++) {
             temporary = array[i];
             for (j = i; j > 0; j--) {
@@ -181,7 +194,9 @@ public class Main {
     static int mergeSort(int array[], int lower, int upper, int k) {
         if (lower >= upper)
             return 0;
+
         int m = (lower + upper) / 2;
+
         mergeSort(array, lower, m, k);
         mergeSort(array, m + 1, upper, k);
         merge(array, lower, upper);
@@ -197,11 +212,15 @@ public class Main {
         int a[] = new int[m - lower + 1];
         int b[] = new int[upper - m];
         int i, k = 0, k1 = 0, k2 = 0;
+
         for (i = lower; i <= m; i++, k++)
             a[k] = array[i];
+
         k = 0;
+
         for (; i <= upper; i++, k++)
             b[k] = array[i];
+
         for (i = lower; i <= upper && k1 < m - lower + 1 && k2 < upper - m; i++) {
             if (a[k1] < b[k2]) {
                 array[i] = a[k1];
@@ -211,6 +230,7 @@ public class Main {
                 k2++;
             }
         }
+
         for (; k1 < m - lower + 1; k1++)
             array[i++] = a[k1];
         for (; k2 < upper - m; k2++)
@@ -221,32 +241,25 @@ public class Main {
     ////////// QUICK SORT ////////////////
     //////////////////////////////////////
 
-    static int quickSort(int array[], int lower, int upper, int k) {
-        if (lower >= upper)
-            return 0;
-        int p = partition(array, lower, upper);
-        quickSort(array, lower, p - 1, k);
-        quickSort(array, p + 1, upper, k);
-
-        if(k>array.length)
-            return -1;
+    static int quickSort(int[] array, int lower, int upper, int k){
+        if(lower < upper){
+            int p = partition(array, lower, upper);
+            quickSort(array, lower, p-1, k);
+            quickSort(array, p+1, upper, k);
+        }
 
         return array[k-1];
     }
 
-    private static int partition(int array[], int lower, int upper) {
-        int pivot = array[upper];
-        int j = lower;
-        for (int i = lower; i <= upper; i++) {
-            if (array[i] < pivot) {
-                swap(array, i, j);
-                j++;
-            }
-        }
+    static int partition(int[] array, int lower, int upper){
+        int p = lower, j;
 
-        swap(array, upper, j);
+        for(j=lower+1; j <= upper; j++)
+            if(array[j] < array[lower])
+                swap(array, ++p, j);
 
-        return j;
+        swap(array, lower, p);
+        return p;
     }
 
     //////////////////////////////////////
@@ -254,7 +267,8 @@ public class Main {
     //////////////////////////////////////
 
     static int selectionSort(int array[], int k) {
-        int length = array.length, position, temporary;
+        int length = array.length, position;
+
         for (int i = 0; i < length; i++) {
             position = i;
             for (int j = i + 1; j < length; j++) {
@@ -276,6 +290,7 @@ public class Main {
 
     static int heapSort(int array[], int k) {
         makeMaxHeap(array);
+
         for (int i = array.length - 1; i > 0; i--) {
             swap(array, i, 0);
             heapAdjust(array, 0, i);
@@ -289,6 +304,7 @@ public class Main {
 
     private static void makeMaxHeap(int array[]) {
         int length = array.length;
+
         for (int i = length / 2 - 1; i >= 0; --i) {
             heapAdjust(array, i, length);
         }
@@ -297,6 +313,7 @@ public class Main {
     private static void heapAdjust(int array[], int i, int n) {
         int j = 2 * i + 1;
         int temporary = array[i];
+
         while (j < n) {
             if (j < n - 1 && array[j] < array[j + 1])
                 j++;
@@ -305,6 +322,7 @@ public class Main {
             array[(j - 1) / 2] = array[j];
             j = 2 * j + 1;
         }
+
         array[(j - 1) / 2] = temporary;
     }
 }
